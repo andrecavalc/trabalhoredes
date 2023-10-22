2d5d846f89f7a8e143a2b7957f40ed38fa54aa69
andrecavalc <acas100303@gmail.com>
2023-10-22 13:12:13 -0300

    sending many messages

--- client.py
+++ client.py
@@ -10,5 +10,27 @@ client_socket.connect(("localhost",5555))
 data_from_server=client_socket.recv(1024)
 print(data_from_server.decode("utf-8"))
 
-#estamos encerrando a conexão
-client_socket.close()
+#se o usuario fizer algo de errado
+#exemplo: ctrl+c
+#entao exibe a msg da linha abaixo do except
+try:
+    # numero de sequencia do pacote
+    seq = 0
+    #envio dos pacotes
+    while True:
+        #sequência atual
+        seq+=1
+        #captura msg do usuario
+        msg_inputed = input(f"{seq}º PACOTE A SER ENVIADO: ")
+        #adiciona o numero de sequencia na msg
+        msg_seq = f"[{seq}] {msg_inputed}"
+        #codifica msg de string para bytes
+        msg_encoded = msg_seq.encode("utf-8")
+        #envia msg do cliente para o servidor
+        data_from_server=client_socket.sendall(msg_encoded)
+
+except KeyboardInterrupt:
+    #####################################
+    print("\n\nO cliente encerrou a conexao com o servidor!")
+    #estamos encerrando a conexão
+    client_socket.close()
--- server.py
+++ server.py
@@ -18,6 +18,19 @@ connection,address=server_socket.accept()
 mensagem= "conexão aceita"
 connection.sendall(mensagem.encode("utf-8"))
 
+#recebimento dos pacotes
+while True:
+    #recebe msg do cliente
+    msg_received = connection.recv(1024)
+    #decodifica a msg de bytes para string
+    msg_decoded = msg_received.decode("utf-8")
+    #verifica se a msg ta vazia (ctrl+c)
+    if msg_decoded == "":
+        print("\nO cliente encerrou a conexao com o servidor!")
+        break
+    #printa msg decodificada
+    print(msg_decoded)
+
 #estamos encerrando a conexão
 server_socket.close()
 connection.close()
